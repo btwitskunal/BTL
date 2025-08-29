c6b3d0c158a40d0def7d1dc097b2f0b62e81d828
KunalSCL <kunal.garg-int@shreecement.com>
2025-08-29 15:18:30 +0530

    Delete

--- backend/routes/analysis.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const analysisController = require('../controllers/analysisController');
-
-router.get('/data', analysisController.getData);
-router.post('/summary', analysisController.getSummary);
-
-module.exports = router; 
--- backend/routes/template.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const templateController = require('../controllers/templateController');
-
-router.get('/download', templateController.downloadTemplate);
-router.get('/', templateController.getTemplate);
-router.post('/update', templateController.updateTemplate);
-
-module.exports = router; 
